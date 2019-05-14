1784c9e5898281a7d96f4ee0e015382ec52eb7aa
Augusitin <50617184+Augusitin@users.noreply.github.com>
2019-05-14 11:22:31 +0200

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>SIO1 - TP Bibliothèque</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
+    <link href="bibliotheque.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+</body>
