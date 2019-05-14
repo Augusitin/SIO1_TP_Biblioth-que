a72affd031725442899f84511de9300bb786e9c6
Jefaiscecomptejustepourlesetudes <50617944+Jefaiscecomptejustepourlesetudes@users.noreply.github.com>
2019-05-14 11:28:18 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="UTF-8">
     <link href="bibliotheque.css" rel="stylesheet"/head>
-za
+
 <body>
 
 </body>
