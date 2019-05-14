26a6febb71f1918856b9ac4de30ac7dc9275a5b4
nicolaspiela <50617739+nicolaspiela@users.noreply.github.com>
2019-05-14 11:27:59 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -5,10 +5,8 @@
     <title>SIO1 - TP Bibliothèque</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="UTF-8">
-    <link href="bibliotheque.css" rel="stylesheet">
-
-</head>
-
+    <link href="bibliotheque.css" rel="stylesheet"/head>
+za
 <body>
 
 </body>
