0ac7cea5eb20075979669c8888d9cbe62394af0f
nicolaspiela <50617739+nicolaspiela@users.noreply.github.com>
2019-05-14 12:41:48 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -8,5 +8,67 @@
     <link href="bibliotheque.css" rel="stylesheet"/head>
 
 <body>
-	<h1>Ma bibliothèque<h1>
+	<header>
+	<h1>Ma bibliothèque</h1>
+	</header>
+	<main>
+		<ul class="bloc1">
+			<img src="img/1.jpg">
+			<li class="titlelivre">
+				php
+
+			</li>
+			<li class="auteur">
+				<p>Ecrit par</p>
+				php
+			</li>
+			<li class="editeur">
+				<p>Edité par</p>
+				php
+			</li>
+		</ul>
+
+		<ul class="bloc2">
+				<img src="">
+			<li class="titlelivre">
+				php
+			</li>
+			<li class="auteur">
+				<p>Ecrit par</p>
+				php
+			</li>
+			<li class="editeur">
+				<p>Edité par</p>
+				php
+			</li>
+		</ul>
+
+		<ul class="bloc3">
+			<li class="titlelivre">
+				php
+			</li>
+			<li class="auteur">
+				<p>Ecrit par</p>
+				php
+			</li>
+			<li class="editeur">
+				<p>Edité par</p>
+				php
+			</li>
+		</ul>
+
+		<ul class="bloc4">
+			<li class="titlelivre">
+				php
+			</li>
+			<li class="auteur">
+				<p>Ecrit par</p>
+				php
+			</li>
+			<li class="editeur">
+				<p>Edité par</p>
+				php
+			</li>
+		</ul>
+	</main>
 </body>
